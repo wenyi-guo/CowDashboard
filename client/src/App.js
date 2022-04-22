d575df6c09256fe9c4f06a5e892d31f14c649434
Wenyi Guo <1540778560@qq.com>
2022-04-22 03:30:43 -0400

    Added dashboard templates

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
     const [data, setData] = useState({})
+    var milkContainer = document.getElementById("milk")
 
     useEffect(() => {
-      fetch("/members").then(
-          res => res.json()
-      ).then(
-          data => {
-            setData(data)
-            console.log(data)
-          }
-      )
+        fetch("/members").then(
+            res => res.json()
+        ).then(
+            data => {
+                setData(data)
+                console.log(data)
+            }
+        )
     }, [])
 
     return (
         <div>
-            {(typeof data.cows === 'undefined')? (
+            {(typeof data.cows === 'undefined') ? (
                 <p> Loading ... </p>
             ) : (
                 data.cows.map((member, i) => (
                     <p key={i}>{JSON.stringify(member)}</p>
-                )) 
+                ))
             )}
 
-            
+
         </div>
     )
 }
