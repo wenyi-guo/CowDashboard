d6774e04c3841bf36845d6465a32dd1301b71850
Wenyi Guo <1540778560@qq.com>
2022-04-29 15:05:30 -0400

    update endoints

--- server.py
+++ server.py
@@ -2,13 +2,19 @@ from flask import Flask
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
 endpoint = "https://diaryinstance.documents.azure.com:443/"
+
 key = 'y5ZatCRxHYVOmYkBpwfP1nJV9yiR5zzyULXetVwZlG97CRUvQ46hfIXg9e9Nlv84aR0742L1YoIMao11NB8GmA=='
 client = CosmosClient(endpoint, key)
 database_name = "dairyDB"
 database = client.create_database_if_not_exists(id=database_name)
-container_name = "weather"
-container = database.create_container_if_not_exists(
-    id=container_name,
+
+weather_container = database.create_container_if_not_exists(
+    id="weather",
+    partition_key=PartitionKey(path="/id"),
+    offer_throughput=400
+)
+milk_container = database.create_container_if_not_exists(
+    id="daily_milk",
     partition_key=PartitionKey(path="/id"),
     offer_throughput=400
 )
@@ -18,15 +24,27 @@ app = Flask(__name__)
 
 
 @app.route("/weather")
-def members():
+def weather():
     #query = """SELECT c.datesql, c.Animal_ID, c.Group_ID, c.AnimalStatus, c.Gynecology_Status, c["Activity(steps/hr)"], c["Weight(gr)"], c["Yield(gr)"] FROM c WHERE c.Animal_ID = 1"""
     query = """SELECT c.Date, c.highest_temp, c.lowest_temp, c.avg_temp, c.highest_thi, c.lowest_thi, c.avg_thi, c.num_records_on_day FROM c"""
-    items = list(container.query_items(
+    items = list(weather_container.query_items(
         query=query,
         enable_cross_partition_query=True
     ))
+    print(type(items[0]['Date']))
+    return {"weather": items}
 
-    return {"cows": items}
+
+@app.route("/milk")
+def milk():
+    #query = """SELECT c.datesql, c.Animal_ID, c.Group_ID, c.AnimalStatus, c.Gynecology_Status, c["Activity(steps/hr)"], c["Weight(gr)"], c["Yield(gr)"] FROM c WHERE c.Animal_ID = 1"""
+    query = """SELECT c.datesql, c.Lactation_Num, c["Yield(gr)"], c.Cow_Num FROM c"""
+    items = list(milk_container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
+    print(type(items[0]['datesql']))
+    return {"milk": items}
 
 
 if __name__ == "__main__":
