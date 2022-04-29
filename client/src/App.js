2bc7c9d5fcf409d96a8d850c624cfdd2c184e2ec
Wenyi Guo <1540778560@qq.com>
2022-04-29 17:46:34 -0400

    line chart

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { Table } from 'antd';
 // import 'antd/dist/antd.less';
 import 'antd/dist/antd.css';
-
+import { DualAxes } from '@ant-design/plots';
 
 
 function App() {
     const [data, setData] = useState({})
-
+    console.log("-----here1")
     useEffect(() => {
-        fetch("/weather").then(
+        fetch("/sum").then(
             res => res.json()
         ).then(
             data => {
                 setData(data)
+                console.log("-----here2")
                 console.log(data)
             }
         )
@@ -42,20 +43,135 @@ function App() {
 
 
         return <Table columns={columns} dataSource={tableData} onChange={onChange} />
-
-
     }
+
+    const DemoDualAxes = () => {
+        const dataChart = data.sum
+        const cow = [
+            {
+                "Yield(gr)": 1160714,
+                "Date": 1521432000
+            },
+            {
+
+                "Yield(gr)": 257949,
+                "Date": 1521432022
+            },
+            {
+                "Yield(gr)": 85665,
+                "Date": 1521432003
+            },
+        ]
+        const testdata = [
+            {
+                year: '1991',
+                value: 3,
+                count: 10,
+            },
+            {
+                year: '1992',
+                value: 4,
+                count: 4,
+            },
+            {
+                year: '1994',
+                value: 5,
+                count: 5,
+            },
+        ];
+        const config = {
+            data: [dataChart, dataChart],
+            xField: 'Date',
+            yField: ['avg_Yield(gr)', 'avg_temp'],
+            // data: [weather, cow],
+            // xField: 'Date',
+            // yField: ['avg_temp', 'Yield(gr)'],
+            geometryOptions: [
+                {
+                    geometry: 'line',
+                    smooth: true,
+                    color: '#5B8FF9',
+                    // label: {
+                    //     formatter: (datum) => {
+                    //         return `${datum.value}个`;
+                    //     },
+                    // },
+                    lineStyle: {
+                        lineWidth: 3,
+                        // lineDash: [5, 5],
+                    },
+                },
+                {
+                    geometry: 'line',
+                    smooth: true,
+                    color: '#5AD8A6',
+                    lineStyle: {
+                        lineWidth: 3,
+                        // opacity: 0.5,
+                    },
+                    // label: {
+                    //     formatter: (datum) => {
+                    //         return `${datum.count}个`;
+                    //     },
+                    // },
+                    // point: {
+                    //     shape: 'circle',
+                    //     size: 4,
+                    //     style: {
+                    //         opacity: 0.5,
+                    //         stroke: '#5AD8A6',
+                    //         fill: '#fff',
+                    //     },
+                    // },
+                },
+            ],
+        };
+        return <DualAxes {...config} />;
+    };
 
     return (
         <div>
-            {(typeof data.weather === 'undefined') ? (
-                <p> Loading ... </p>
-            ) :
-                renderTable()
-            }
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Milk Daily Overview</h6>
+                        </div>
+                        <div class="card-body">
+                            <div>
+                                {(typeof data.weather === 'undefined') ? (
+                                    <p> Loading ... </p>
+                                ) : renderTable()
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Milk Daily vs Weather Chart</h6>
+                        </div>
+                        <div class="card-body">
+                            <div>
+                                {(typeof data.weather === 'undefined') ? (
+                                    <p> Loading ... </p>
+                                ) :
+                                    <DemoDualAxes />
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
+
+
+
     )
 }
 
