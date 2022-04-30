ce976ec1624ad8314318afc00af15f1f4a11c40a
Wenyi Guo <1540778560@qq.com>
2022-04-30 00:18:25 -0400

    added tables and clean up

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,28 @@ function App() {
         console.log('params', pagination, filters, sorter, extra);
     }
 
-    const renderTable = () => {
+    const renderTableMilk = () => {
+        console.log("data is ", Object.keys(data.milk[0]))
+        const columns = Object.keys(data.milk[0]).map((mem, i) => {
+            console.log("-------", mem, i)
+            return {
+                title: mem,
+                dataIndex: mem,
+                key: mem,
+                sorter: (a, b) => a[mem] - b[mem],
+                defaultSortOrder: mem === 'Date' ? 'descend' : console.log()
+            }
+        })
+        const tableData = data.milk
+
+        console.log("columns ", columns)
+        // console.log("tableData ", tableData)
+
+
+        return <Table columns={columns} dataSource={tableData} onChange={onChange} />
+    }
+
+    const renderTableWeather = () => {
         console.log("data is ", Object.keys(data.weather[0]))
         const columns = Object.keys(data.weather[0]).map((mem, i) => {
             console.log("-------", mem, i)
@@ -47,38 +68,6 @@ function App() {
 
     const DemoDualAxes = () => {
         const dataChart = data.sum
-        const cow = [
-            {
-                "Yield(gr)": 1160714,
-                "Date": 1521432000
-            },
-            {
-
-                "Yield(gr)": 257949,
-                "Date": 1521432022
-            },
-            {
-                "Yield(gr)": 85665,
-                "Date": 1521432003
-            },
-        ]
-        const testdata = [
-            {
-                year: '1991',
-                value: 3,
-                count: 10,
-            },
-            {
-                year: '1992',
-                value: 4,
-                count: 4,
-            },
-            {
-                year: '1994',
-                value: 5,
-                count: 5,
-            },
-        ];
         const config = {
             data: [dataChart, dataChart],
             xField: 'Date',
@@ -115,9 +104,27 @@ function App() {
                         </div>
                         <div class="card-body">
                             <div>
+                                {(typeof data.milk === 'undefined') ? (
+                                    <p> Loading ... </p>
+                                ) : renderTableMilk()
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Weather Overview</h6>
+                        </div>
+                        <div class="card-body">
+                            <div>
                                 {(typeof data.weather === 'undefined') ? (
                                     <p> Loading ... </p>
-                                ) : renderTable()
+                                ) : renderTableWeather()
                                 }
                             </div>
                         </div>
@@ -133,7 +140,7 @@ function App() {
                         </div>
                         <div class="card-body">
                             <div>
-                                {(typeof data.weather === 'undefined') ? (
+                                {(typeof data.sum === 'undefined') ? (
                                     <p> Loading ... </p>
                                 ) :
                                     <DemoDualAxes />
--- server.py
+++ server.py
@@ -41,12 +41,12 @@ def weather():
 @app.route("/milk")
 def milk():
     #query = """SELECT c.datesql, c.Animal_ID, c.Group_ID, c.AnimalStatus, c.Gynecology_Status, c["Activity(steps/hr)"], c["Weight(gr)"], c["Yield(gr)"] FROM c WHERE c.Animal_ID = 1"""
-    query = """SELECT c.datesql, c.Lactation_Num, c["Yield(gr)"], c.Cow_Num FROM c"""
+    query = """SELECT c.Date, c.Lactation_Num, c["Yield(gr)"], c.Cow_Num FROM c"""
     items = list(milk_container.query_items(
         query=query,
         enable_cross_partition_query=True
     ))
-    print(type(items[0]['datesql']))
+    print(type(items[0]['Date']))
     return {"milk": items}
 
 
@@ -58,6 +58,7 @@ def milk_sum():
         enable_cross_partition_query=True
     ))
     milk_df = pd.read_json(json.dumps(items_milk), orient='records')
+    milk_df['Date'] = milk_df['Date'].dt.strftime('%Y-%m-%d')
 
     query_weather = """SELECT c.Date, c.highest_temp, c.lowest_temp, c.avg_temp, c.highest_thi, c.lowest_thi, c.avg_thi, c.num_records_on_day FROM c"""
     items_weather = list(weather_container.query_items(
@@ -65,18 +66,30 @@ def milk_sum():
         enable_cross_partition_query=True
     ))
     weather_df = pd.read_json(json.dumps(items_weather), orient='records')
+    weather_df['Date'] = weather_df['Date'].dt.strftime('%Y-%m-%d')
 
     aggregation_functions = {'Lactation_Num': 'first',
                              'Yield(gr)': 'sum', 'Cow_Num': 'sum'}
-    milk_df = milk_df.groupby(milk_df['Date']).aggregate(aggregation_functions)
-    df = pd.merge(milk_df, weather_df, on="Date")
+    milk_df_temp = milk_df.groupby(
+        milk_df['Date']).aggregate(aggregation_functions)
+    df = pd.merge(milk_df_temp, weather_df, on="Date")
     df = df.drop(['Lactation_Num'], axis=1)
-    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
     df['avg_Yield(gr)'] = (df['Yield(gr)']/df['Cow_Num'])
+    df = df.astype({"avg_Yield(gr)": int})
 
-    result = df.to_json(orient="records")
-    parsed = json.loads(result)
-    return {"milk": items_milk, "weather": items_weather, "sum": parsed}
+    result_sum = df.to_json(orient="records")
+    parsed_sum = json.loads(result_sum)
+
+    milk_df['avg_Yield(gr)'] = (milk_df['Yield(gr)']/milk_df['Cow_Num'])
+    milk_df = milk_df.astype({"avg_Yield(gr)": int})
+    milk_df = milk_df.drop(['Cow_Num'], axis=1)
+    result_milk = milk_df.to_json(orient="records")
+    parsed_milk = json.loads(result_milk)
+
+    result_weather = weather_df.to_json(orient="records")
+    parsed_weather = json.loads(result_weather)
+
+    return {"milk": parsed_milk, "weather": parsed_weather, "sum": parsed_sum}
 
 
 if __name__ == "__main__":
